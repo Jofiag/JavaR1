93a6527313ea5843ec0dfcafe064483407d0746d
Jofiag <Aguemon.wahab.97@gmail.com>
2019-06-24 21:01:05 +0200

    Salutations de John

--- src/com/opc/jofiag/Main.java
+++ src/com/opc/jofiag/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello OpenClassrooms !! ko");
+        System.out.println("Salut tous le monde! Heureux de faire votre connaissance!!!");
     }
 }
