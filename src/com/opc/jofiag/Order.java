98c8f636341a208b88c02773e501777f9077df7b
Jofiag <Aguemon.wahab.97@gmail.com>
2019-06-24 22:18:35 +0200

    Implémentation des fonctions displayAvailableMenu et displaySelectedMenu

--- src/com/opc/jofiag/Order.java
+++ src/com/opc/jofiag/Order.java
@@ -1,4 +1,26 @@
 package com.opc.jofiag;
 
 public class Order {
+
+    /**
+     * Display all available menu in the restaurant.
+     */
+    public void displayAvailableMenu()
+    {
+        System.out.println("Choix du menu");
+        System.out.println("1-Poulet");
+        System.out.println("2-Boeuf");
+        System.out.println("3-Vegetarien");
+        System.out.println("Quel est votre choix");
+    }
+
+    /**
+     * Display menu selected.
+     * @param menuNb the selected menu.
+     */
+
+    public void displayMenuSelected(int menuNb)
+    {
+        System.out.println("Vous avez choisi le menu " + menuNb);
+    }
 }
